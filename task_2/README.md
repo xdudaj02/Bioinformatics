3a072646337315a9dd125e9ae567df2d66a5f341
xdudaj02 <83180655+xdudaj02@users.noreply.github.com>
2024-03-13 14:04:27 +0000

    second individual assignment

---
+++ task_2/README.md
@@ -0,0 +1,15 @@
+# Bioinformatics - Individual Assignment 2
+Python code for the second individual assignment for the Bioinformatics cours at FCUP 23/24. Solves the third problem of the assignment.
+
+## Usage
+Script requires the `sequence_alignments.py` file from the fifth lab of the course to be in the same directory.
+
+## Output
+The script will output the following information about the sequence alignment for both global and local alignment:
+- The score matrix
+- The traceback matrix
+- The optimal alignment score
+- The optimal alignment
+
+## Issues
+The fourth tasks of the first and second problems can not be solved with functions from the provided file. Currently there is no solution for this.
---
+++ task_2/solution.py
@@ -0,0 +1,42 @@
+from sequence_alignments import *
+
+SEQ_1 = 'TATTCG'
+SEQ_2 = 'ATTTCC'
+MATCH = 2
+MISMATCH = -1
+GAP = -3
+
+def get_alignments(seq_1 : str = SEQ_1, seq_2 : str = SEQ_2, match : int = MATCH, mismatch : int = MISMATCH, gap : int = GAP):
+    '''
+    Given two sequences and the match, mismatch, and gap scores, print all the relevant information about the global and local alignments.
+
+    Args:
+    - seq_1 (str): the first sequence to be aligned
+    - seq_2 (str): the second sequence to be aligned
+    - match (int): the score for a match
+    - mismatch (int): the score for a mismatch
+    - gap (int): the score for a gap
+    '''
+    # Determine the score and traceback matrix. Determine the best score.
+    sub_matrix = create_submat(match, mismatch)
+    global_score_matrix, global_traceback_matrix = needleman_Wunsch(seq_1, seq_2, sub_matrix, gap)
+    global_best_score = global_score_matrix[-1][-1]
+    local_score_matrix, local_traceback_matrix, local_best_score = smith_Waterman(seq_1, seq_2, sub_matrix, gap)
+
+    print('Global score matrix:', global_score_matrix)
+    print('Global traceback matrix:', global_traceback_matrix)
+    print('Global alignment score:', global_best_score)
+    print('Local score matrix:', local_score_matrix)
+    print('Local traceback matrix:', local_traceback_matrix)
+    print('Local alignment score:', local_best_score)
+    
+    # Retrieve the optimal sequence alignment.
+    optimal_global_alignment = recover_align(global_traceback_matrix, seq_1, seq_2)
+    optimal_local_alignment = recover_align_local(local_score_matrix, local_traceback_matrix, seq_1, seq_2)
+
+    print('Optimal global alignment:', optimal_global_alignment)
+    print('Optimal local alignment:', optimal_local_alignment)
+    
+    # Explain if there are multiple best alignments
+    # TODO: ???
+    # not sure what to do here as it cant really be done with the provided functions and the solution would probably be too complex to fit on one page
